fa48c2767f60028868c90dc54bb838feda0b49e3
EricVentura <ericvent@outlook.com>
2023-04-01 12:21:58 -0400

    test pass

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 22;
+const num4 = 6;
+const mod = num1 % num2;
+
+const max = Math.max(num1, num4, 20);
